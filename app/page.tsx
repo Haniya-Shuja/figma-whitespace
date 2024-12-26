570ea85a68be8b125f7e9cb676a8741aab904554
Haniya-Shuja <haniyashuja786@gmail.com>
2024-12-26 12:56:09 -0800

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <h1>My-app</h1>
+    <div>
+      <h1>Project</h1>
+      <h1>Management</h1>
+      <p>this is the figma template</p>
+      <Image 
+        src="/image/img-1.png" // Replace with the actual path to your image
+        alt="Description of your image" 
+        width={500} // Adjust width as needed
+        height={300} // Adjust height as needed 
+      />
+    </div>
   );
 }
